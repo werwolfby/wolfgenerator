cb2520ad0ff2eab4adca9332bdaaa2b2b44060e8
WerWolf <WerWolf@3d5925fb-d3ef-a74d-b267-2fbeb622cb46>
2009-01-26 16:59:27 +0000

    ! Use BaseCodeGeneratorWithSite.

--- WolfGenerator.Core/WolfGeneratorCustomToolGenerator.cs
+++ WolfGenerator.Core/WolfGeneratorCustomToolGenerator.cs
@@ -1,15 +1,14 @@
 using System;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
-using Microsoft.VisualStudio.Shell.Interop;
+using CustomToolGenerator;
 using WolfGenerator.Core.Writer;
 
 namespace WolfGenerator.Core
 {
     [Guid( "02F8A379-7EBA-420C-AFAD-9B8E98DA8562" )]
     [ComVisible( true )]
-    public class CustomToolGenerator : IVsSingleFileGenerator
+    public class WolfGeneratorCustomToolGenerator : BaseCodeGeneratorWithSite
     {
         #region IVsSingleFileGenerator Members
         public int DefaultExtension( out string pbstrDefaultExtension )
@@ -17,15 +16,14 @@ namespace WolfGenerator.Core
             pbstrDefaultExtension = ".cs";
             return 0;
         }
+    	#endregion
 
-        public int Generate( string wszInputFilePath, string bstrInputFileContents, string wszDefaultNamespace,
-                             IntPtr[] rgbOutputFileContents,
-                             out uint pcbOutput, IVsGeneratorProgress pGenerateProgress )
-        {
+    	protected override byte[] GenerateCode( string inputFileName, string inputFileContent )
+    	{
         	string programCode;
 			try
 			{
-				var scanner = new Scanner( wszInputFilePath );
+				var scanner = new Scanner( inputFileName );
 				var parser = new Parser( scanner );
 
 				parser.Parse();
@@ -44,15 +42,7 @@ namespace WolfGenerator.Core
 				programCode = "// Exception: " + e.Message;
 			}
 
-        	byte[] bytes = Encoding.GetEncoding( 1251 ).GetBytes( programCode );
-
-        	rgbOutputFileContents[0] = Marshal.AllocCoTaskMem( bytes.Length );
-			Marshal.Copy( bytes, 0, rgbOutputFileContents[0], bytes.Length );
-
-        	pcbOutput = (uint)bytes.Length;
-
-        	return 0;
-        }
-    	#endregion
+    		return Encoding.UTF8.GetBytes( programCode );
+    	}
     }
 }

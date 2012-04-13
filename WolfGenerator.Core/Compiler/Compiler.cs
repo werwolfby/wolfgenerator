d0341638dfc0c9627b4c96edcd488da8825ce716
aliaksandr.puzynia <aliaksandr.puzynia@043e2f95-8c97-a241-8f02-4735e6389524>
2012-04-13 13:53:32 +0300

    [+] Add [Compiler].

---
+++ WolfGenerator.Core/Compiler/Compiler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.CSharp;
+using WolfGenerator.Core.CodeGenerator;
+
+namespace WolfGenerator.Core.Compiler
+{
+	public class Compiler
+	{
+		private readonly string fileName;
+		private readonly CompilerParameters _compilerParams = new CompilerParameters
+		                                                      {
+		                                                      	GenerateExecutable = false,
+		                                                      	GenerateInMemory = true,
+		                                                      	OutputAssembly = "Generated.dll"
+		                                                      };
+
+		public Compiler(string fileName)
+		{
+			this.fileName = fileName;
+		}
+
+		public void AddReference(string assemblyName)
+		{
+			_compilerParams.ReferencedAssemblies.Add(assemblyName);
+		}
+
+		public Assembly Compile()
+		{
+			var options = new Dictionary<string, string>
+			              {
+			              	{"CompilerVersion", "v3.5"}
+			              };
+			var codeProvider = new CSharpCodeProvider(options);
+
+			var scanner = new Scanner(this.fileName);
+			var parser = new Parser(scanner);
+
+			parser.Parse();
+
+			var generatator = new Generator();
+			var generatedCode = generatator.Generate(null, parser.ruleClassStatement, "");
+
+			var result = codeProvider.CompileAssemblyFromSource(this._compilerParams, generatedCode.ToString());
+
+			return result.CompiledAssembly;
+		}
+	}
+}

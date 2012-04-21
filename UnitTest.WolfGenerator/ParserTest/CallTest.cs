7859e9e286e522a2136e1a878a4b7dfca737b82a
Alexander Puzynia <werwolf.by@gmail.com>
2012-04-21 22:04:07 +0300

    [+] Add [EmptyAndNullParametersTest] test.

--- UnitTest.WolfGenerator/ParserTest/CallTest.cs
+++ UnitTest.WolfGenerator/ParserTest/CallTest.cs
@@ -14,6 +14,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WolfGenerator.Core.AST;
 using WolfGenerator.Core.CodeGenerator;
+using WolfGenerator.Core.Invoker;
+using WolfGenerator.Core.Writer;
 
 namespace UnitTest.WolfGenerator.ParserTest
 {
@@ -24,16 +26,40 @@ namespace UnitTest.WolfGenerator.ParserTest
 		public void SimpleTest()
 		{
 			var generator = new Generator();
+			var dynamicInvoker = new DynamicInvoker( generator );
 
 			var callStatement = new CallStatement( Helper.EmptyPosition, "Name", "param1, param2" );
 
-			var invokeCodeWriter = generator.GenerateInvoke( callStatement );
+			var invokeCodeWriter = dynamicInvoker.Invoke<CodeWriter>( "GenerateInvoke", callStatement );
 
 			Assert.IsNotNull( invokeCodeWriter );
 
 			var invokeText = invokeCodeWriter.ToString().Replace( " ", "" );
 
 			Assert.AreEqual( "this.Invoke(\"Name\",param1,param2)", invokeText );
+		}
+
+		[TestMethod]
+		public void EmptyAndNullParametersTest()
+		{
+			var generator = new Generator();
+			var dynamicInvoker = new DynamicInvoker( generator );
+
+			EmptyAndNullParametersTestHelper( dynamicInvoker, "" );
+			EmptyAndNullParametersTestHelper( dynamicInvoker, null );
+		}
+
+		private static void EmptyAndNullParametersTestHelper( DynamicInvoker dynamicInvoker, string parameters )
+		{
+			var callStatement = new CallStatement( Helper.EmptyPosition, "Name", parameters );
+
+			var invokeCodeWriter = dynamicInvoker.Invoke<CodeWriter>( "GenerateInvoke", callStatement );
+
+			Assert.IsNotNull( invokeCodeWriter );
+
+			var invokeText = invokeCodeWriter.ToString().Replace( " ", "" );
+
+			Assert.AreEqual( "this.Invoke(\"Name\")", invokeText );
 		}
 	}
 }

e66cfc9927a755ed1453bfce8dcd121e08fa835e
Alexander Puzynia <werwolf.by@gmail.com>
2012-04-21 09:54:48 +0300

    [+] Add [InvokeTests] with [InvokeTests].

---
+++ UnitTest.WolfGenerator/GeneratorTest/InvokeTests.cs
@@ -0,0 +1,43 @@
+﻿/*******************************************************
+ *
+ * Created by: Alexander Puzynia aka WerWolf
+ * Created: 21.04.2012 08:28
+ *
+ * File: InvokeTests.cs
+ * Remarks:
+ * 
+ * History:
+ *   21.04.2012 08:28 - Create Wireframe
+ *
+ *******************************************************/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.WolfGenerator.GeneratorTest
+{
+	[TestClass]
+	public class InvokeTests
+	{
+		[TestMethod]
+		public void SimpleInvokeTest()
+		{
+			var testGenerator = new TestGenerator();
+
+			var typeProperty = new TypeProperty { Name = "Property1", Type = typeof(int) };
+
+			testGenerator.CallDefineProperty( typeProperty );
+
+			Assert.AreEqual( 1, testGenerator.TypeDefinePropertyCalls );
+			Assert.AreEqual( 0, testGenerator.NavigationDefinePropertyCalls );
+
+			testGenerator.TypeDefinePropertyCalls = 0;
+
+			var navigationProperty = new NavigationProperty { Name = "Property2", Property = typeProperty, IsCollection = true };
+
+			testGenerator.CallDefineProperty( navigationProperty );
+
+			Assert.AreEqual( 0, testGenerator.TypeDefinePropertyCalls );
+			Assert.AreEqual( 1, testGenerator.NavigationDefinePropertyCalls );
+		}
+	}
+}
---
+++ UnitTest.WolfGenerator/GeneratorTest/TestGenerator.cs
@@ -0,0 +1,57 @@
+﻿/*******************************************************
+ *
+ * Created by: Alexander Puzynia aka WerWolf
+ * Created: 21.04.2012 08:29
+ *
+ * File: TestGenerator.cs
+ * Remarks:
+ * 
+ * History:
+ *   21.04.2012 08:29 - Create Wireframe
+ *
+ *******************************************************/
+
+using System;
+using WolfGenerator.Core;
+
+namespace UnitTest.WolfGenerator.GeneratorTest
+{
+	public abstract class Property
+	{
+		public string Name { get; set; }
+	}
+
+	public class TypeProperty : Property
+	{
+		public Type Type { get; set; }
+	}
+
+	public class NavigationProperty : Property
+	{
+		public Property Property { get; set; }
+
+		public bool IsCollection { get; set; }
+	}
+
+	public class TestGenerator : GeneratorBase
+	{
+		public int TypeDefinePropertyCalls { get; set; }
+		
+		public int NavigationDefinePropertyCalls { get; set; }
+
+		public void DefineProperty( TypeProperty typeProperty )
+		{
+			this.TypeDefinePropertyCalls++;
+		}
+
+		public void DefineProperty( NavigationProperty navigationProperty )
+		{
+			this.NavigationDefinePropertyCalls++;
+		}
+
+		public void CallDefineProperty( Property property )
+		{
+			this.Invoke( "DefineProperty", property );
+		}
+	}
+}

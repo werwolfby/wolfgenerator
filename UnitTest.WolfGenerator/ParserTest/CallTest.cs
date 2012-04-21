71e1acf0c911efabfb522970c401be0a085998ce
Alexander Puzynia <werwolf.by@gmail.com>
2012-04-21 21:55:39 +0300

    [+] Add [CallTest].

---
+++ UnitTest.WolfGenerator/ParserTest/CallTest.cs
@@ -0,0 +1,39 @@
+﻿/*******************************************************
+ *
+ * Created by: Alexander Puzynia aka WerWolf
+ * Created: 21.04.2012 21:52
+ *
+ * File: CallTest.cs
+ * Remarks:
+ * 
+ * History:
+ *   21.04.2012 21:52 - Create Wireframe
+ *
+ *******************************************************/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WolfGenerator.Core.AST;
+using WolfGenerator.Core.CodeGenerator;
+
+namespace UnitTest.WolfGenerator.ParserTest
+{
+	[TestClass]
+	public class CallTest
+	{
+		[TestMethod]
+		public void SimpleTest()
+		{
+			var generator = new Generator();
+
+			var callStatement = new CallStatement( Helper.EmptyPosition, "Name", "param1, param2" );
+
+			var invokeCodeWriter = generator.GenerateInvoke( callStatement );
+
+			Assert.IsNotNull( invokeCodeWriter );
+
+			var invokeText = invokeCodeWriter.ToString().Replace( " ", "" );
+
+			Assert.AreEqual( "this.Invoke(\"Name\",param1,param2)", invokeText );
+		}
+	}
+}

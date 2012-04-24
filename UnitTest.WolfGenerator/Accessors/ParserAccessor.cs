f4e23fd495313f8152800ddc3b251d2e58613f04
Alexander Puzynia <werwolf.by@gmail.com>
2012-04-24 16:48:56 +0300

    [+] Add other methods.

--- UnitTest.WolfGenerator/Accessors/ParserAccessor.cs
+++ UnitTest.WolfGenerator/Accessors/ParserAccessor.cs
@@ -8,6 +8,8 @@
  * 
  * History:
  *   24.04.2012 15:50 - Create Wireframe
+ *   24.04.2012 16:43 - [*] Extract common [Call] method.
+ *   24.04.2012 16:48 - [+] Add other methods.
  *
  *******************************************************/
 
@@ -25,15 +27,59 @@ namespace UnitTest.WolfGenerator.Accessors
 		public ParserAccessor( string source )
 		{
 			this.parser = new Parser( source );
+		}
+
+		public void InitParse()
+		{
 			this.parser.InitParse();
 		}
 
-		public void Type( out Type type )
+		public void Type( out Type statement )
 		{
-			var method = parserType.GetMethod( "Type", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public );
+			this.Call( "Type", out statement );
+		}
+
+		public void Value( out ValueStatement statement )
+		{
+			this.Call( "Value", out statement );
+		}
+
+		public void Var( out Variable statement )
+		{
+			this.Call( "Var", out statement );
+		}
+
+		public void Apply( out ApplyStatement statement )
+		{
+			this.Call( "Apply", out statement );
+		}
+
+		public void MatchMethod( out MatchMethodStatement statement )
+		{
+			this.Call( "MatchMethod", out statement );
+		}
+
+		public void Join( out JoinStatement statement )
+		{
+			this.Call( "Join", out statement );
+		}
+
+		public void RuleMethod( out RuleMethodStatement statement )
+		{
+			this.Call( "RuleMethod", out statement );
+		}
+
+		public void Using( out UsingStatement statement )
+		{
+			this.Call( "Using", out statement );
+		}
+
+		private void Call<T>( string name, out T type )
+		{
+			var method = this.parserType.GetMethod( name, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public );
 			var args = new object[1];
-			method.Invoke( parser, args );
-			type = (Type)args[0];
+			method.Invoke( this.parser, args );
+			type = (T)args[0];
 		}
 	}
 }

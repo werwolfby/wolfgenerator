3e0182779e9b976b34a5c8e9f244e122f96b71af
Alexander Puzynia <werwolf.by@gmail.com>
2013-02-06 20:38:37 +0300

    [*] Update version to [2.3.3.0].

--- WolfGenerator.Core/Properties/AssemblyInfo.cs
+++ WolfGenerator.Core/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid( "369dfaaf-eb0a-44e6-a36e-5f3e9151d450" )]
 
-[assembly: AssemblyVersion( "2.3.2.1" )]
-[assembly: AssemblyFileVersion( "2.3.2.1" )]
+[assembly: AssemblyVersion( "2.3.3.0" )]
+[assembly: AssemblyFileVersion( "2.3.3.0" )]
